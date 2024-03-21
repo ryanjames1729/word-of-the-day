bf7c07a0de40e6951107a10d8cb3f5fcdfe23b03
Ryan James <Ryan James>
2024-03-21 15:25:37 -0400

    working on performance

--- src/components/sample.tsx
+++ src/components/sample.tsx
@@ -1,48 +1,53 @@
 import type { isRenderInstruction } from "astro/runtime/server/render/instruction.js";
 import wordList from "./wordList"
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
 
-export default function Sample() {
-    const { width, height } = useWindowSize()
-
+function createWordArray() {
     const words = wordList();
     let scrabbleWords = words.split(" ").filter(word => word.length === 5).join("\n");
-    // remove words that dont have letters
-    scrabbleWords = words.split(" ").filter(word => word.length === 5 && word.match(/[a-zA-Z]/)).join("\n");
-    // build array from scrabbleWords
+    //scrabbleWords = words.split(" ").filter(word => word.length === 5 && word.match(/[a-zA-Z]/)).join("\n");
     let wordArray = scrabbleWords.split("\n");
-    // remove words that dont contain only letters from the array
-    wordArray = wordArray.filter(word => word.match(/^[a-zA-Z]+$/));
-    // shift all to uppercase and remove duplicates
-    wordArray = wordArray.map(word => word.toUpperCase());
     wordArray = wordArray.filter((word, index) => wordArray.indexOf(word) === index);
+    return wordArray;
+}
 
+function createWordDictionary(arr: string[]) {
     // create a dictionary with the date as the key starting with Jan 1, 2024 and the wordArray as the value
     let wordDictionary: { [key: string]: string} = {};
     let date = new Date("2024-01-01");
-    for (let i = 0; i < wordArray.length; i++) {
-        wordDictionary[date.toDateString()] = wordArray[i];
+    for (let i = 0; i < arr.length; i++) {
+        wordDictionary[date.toDateString()] = arr[i];
         date.setDate(date.getDate() + 1);
-    }
+    } 
+    return wordDictionary;
+}
+
+export default function Sample() {
+    const { width, height } = useWindowSize()
+
+    const [wordArray, setWordArray] = useState<string[]>(createWordArray());
+    const [wordDictionary, setWordDictionary] = useState<{ [key: string]: string}>(createWordDictionary(wordArray));
+    
+    useEffect(() => {
+        setWordArray(createWordArray());
+        setWordDictionary(createWordDictionary(wordArray));
+    }, []);
+    
+
 
     const [guessNumber, setGuessNumber] = useState(0);
     const [guess, setGuess] = useState("");
     const [guessArrayBefore, setGuessArrayBefore] = useState<string[]>(["?????", "?????", "?????"]);
     const [guessArrayAfter, setGuessArrayAfter] = useState<string[]>(["[[[[[", "[[[[[", "[[[[["]);
-    const [gameStart, setGameStart] = useState(false);
     const [messageAlert, setMessageAlert] = useState("");
     const [round, setRound] = useState(1);
     const [solutionArray, setSolutionArray] = useState<string[]>(wordDictionary[new Date().toDateString()].split(""));
+    const [solution, setSolution] = useState(wordDictionary[new Date().toDateString()].toUpperCase());
     const [welcomeMsg, setWelcomeMsg] = useState(true);
     const [cookie, setCookie] = useState(document.cookie);
-
-    useEffect(() => {
-        const welcome = setTimeout(() => {setWelcomeMsg(false)}, 30000);
-        return () => clearTimeout(welcome);
-    }, [welcomeMsg]);
 
     return (
         <div className="flex flex-col justify-center place-items-center lg:w-full w-5/6 max-h-screen bg-[#8AA8A1] rounded-lg lg:py-1 pb-1 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#f97316,0_0_15px_#f97316,0_0_30px_#f97316]">
@@ -55,7 +60,7 @@ export default function Sample() {
             <img src="/sample_game.png" alt="Sample Game" className="w-1/4" />
             <p className="p-5">In this example, the solution is alphabetically between DEATH and ROUND and includes the letters OU and H.</p>
 
-            <button className="bg-blue-400 hover:bg-blue-500 hover:text-bold rounded-md p-2 shadow-2xl shadow-inner border-black border-2 border-solid" onClick={()=>{
+            <button className="bg-blue-400 hover:bg-blue-500 hover:text-bold rounded-md p-2 shadow-inner border-black border-2 border-solid" onClick={()=>{
                 setWelcomeMsg(false);
                 document.cookie = "introduction=true; max-age=1209600; path=/";
             }}>Ok, I get it. Let me play.</button>
@@ -69,8 +74,6 @@ export default function Sample() {
         guessNumber === 5 ? '🤦 🤦 🤦 🤦 🤦' : ' '}</p>
         <form onSubmit={(event)=>{
             event.preventDefault();
-            console.log('submit', guess)
-            setGameStart(true);
             if(guessArrayBefore.includes(guess.toUpperCase()) || guessArrayAfter.includes(guess.toUpperCase())) {
                 setMessageAlert("Invalid guess. Please enter a 5 letter word that has not been guessed before.")
             }
@@ -81,25 +84,23 @@ export default function Sample() {
                 setMessageAlert("Invalid guess. Please enter a word that exists in the Scrabble dictionary.")
             }
             else{
-                if(wordDictionary[new Date().toDateString()].localeCompare(guess.toUpperCase()) > 0) {
+                if(solution.localeCompare(guess.toUpperCase()) > 0) {
                     if(guessArrayBefore.indexOf("?????") > -1) {
                         guessArrayBefore.splice(guessArrayBefore.indexOf("?????"), 1);
                     }
                     guessArrayBefore.push(guess.toUpperCase());
                     guessArrayBefore.sort();
-                    // setGuessArrayBefore([...guessArrayBefore.sort(), guess.toUpperCase()]);
                     setGuessArrayBefore(guessArrayBefore.sort())
                     setGuessNumber(guessNumber + 1);
-                } else if(wordDictionary[new Date().toDateString()].localeCompare(guess.toUpperCase()) < 0) {
+                } else if(solution.localeCompare(guess.toUpperCase()) < 0) {
                     if(guessArrayAfter.indexOf("[[[[[") > -1) {
                         guessArrayAfter.splice(guessArrayBefore.indexOf("[[[[["), 1);
                     }
                     guessArrayAfter.push(guess.toUpperCase());
                     guessArrayAfter.sort();
-                    //setGuessArrayAfter([...guessArrayAfter, guess.toUpperCase()]);
                     setGuessArrayAfter(guessArrayAfter.sort());
                     setGuessNumber(guessNumber + 1);
-                } else if(wordDictionary[new Date().toDateString()] === guess.toUpperCase()) {
+                } else if(solution === guess.toUpperCase()) {
                     setMessageAlert("You guessed the word!");
                 }
             }   
@@ -117,7 +118,7 @@ export default function Sample() {
             </ul>
             <input className="text-black text-5xl w-1/2 text-center lg:w-1/4 font-mono uppercase" type="text" id="guess" name="guess" maxLength={5} value={guess} onChange={(event)=>{
                 setGuess(event.target.value);
-                setMessageAlert("");
+                //setMessageAlert("");
             }}/>
             <p>Guesses after the word:</p>
             <ul className="text-4xl">
@@ -128,7 +129,7 @@ export default function Sample() {
             </ul>
             
             <br />
-            {guessNumber < 5 ? <button type="submit" className="bg-blue-400 hover:bg-blue-500 hover:text-bold rounded-md p-2 shadow-2xl shadow-inner border-black border-2 border-solid">Submit</button> : null}
+            {guessNumber < 5 ? <button type="submit" className="bg-blue-400 hover:bg-blue-500 hover:text-bold rounded-md p-2 shadow-inner border-black border-2 border-solid">Submit</button> : null}
             {guessNumber === 5 ? <button type="reset" className="bg-red-400 hover:bg-red-500 hover:text-bold rounded-md p-2 shadow-2xl border-black border-2 border-solid" onClick={()=>{
                 setGuessNumber(0);
                 setGuessArrayBefore(["?????", "?????", "?????"]);
@@ -137,19 +138,14 @@ export default function Sample() {
             }}>Reset</button> : null}
             {/* {messageAlert ? <p className="p-3">{messageAlert}</p> : null} */}
             {guessNumber === 5 ? <p className="p-3">You are out of guesses! Hit reset to try again!</p> : null}
-            {/* {messageAlert === "You guessed the word!" ? <div className="p-2 text-center">
-                <p>
-            Today's word is: {wordDictionary[new Date().toDateString()]}<br />
-            Find out more on Google: <a className="text-orange-500 underline decoration-4" href={`https://www.google.com/search?q=${wordDictionary[new Date().toDateString()].toLowerCase()}+definition`}>{wordDictionary[new Date().toDateString()]}</a>
-        </p></div> : null} */}
 
         {messageAlert === "You guessed the word!" ? <div className="flex flex-col justify-center place-items-center p-2 min-h-48 lg:w-1/2 -mt-10 absolute z-10 mx-1 lg:mx-0 bg-slate-500 text-white rounded-md border-solid border-slate-700">
         {width > 460 ? <Confetti width={width*0.5} height={height*0.7} /> : <Confetti width={width*0.8} height={height*0.8} />}
         
             <h2 className="text-bold text-3xl">Congratulations!</h2>
             <p className="p-5">
-            Today's word is: {wordDictionary[new Date().toDateString()]}<br />
-            Find out more on Google: <a className="text-orange-500 underline decoration-4" href={`https://www.google.com/search?q=${wordDictionary[new Date().toDateString()].toLowerCase()}+definition`}>{wordDictionary[new Date().toDateString()]}</a>
+            Today's word is: {solution}<br />
+            Find out more on Google: <a className="text-orange-500 underline decoration-4" href={`https://www.google.com/search?q=${solution.toLowerCase()}+definition`}>{solution}</a>
             </p>
             <button className="bg-blue-400 hover:bg-blue-500 hover:text-bold rounded-md p-2 shadow-2xl shadow-inner border-black border-2 border-solid" onClick={()=>{
                 setMessageAlert("");
@@ -168,8 +164,7 @@ export default function Sample() {
         <button className="pt-8 decoration-6 underline text-red-700 hover:text-slate-800" onClick={()=>{
             document.cookie = 'introduction=; Max-Age=0; path=/;'
             setCookie(document.cookie);
-            setWelcomeMsg(true);
-            //location.reload();  
+            setWelcomeMsg(true); 
         }}>Help Me Please</button>
         </div>
     );
